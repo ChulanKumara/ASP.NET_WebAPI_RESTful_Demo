795b649504888f1bcf28b5573fec1fa27da86fb5
chulan.kumara <chulan.kumara@DESKTOP-4VO3P04>
2017-09-25 10:01:16 +0530

    Add project files.

---
+++ WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public String Get()
+        {
+            return "Welcome To Web API";
+        }
+
+        public List<string> Get(int id)
+        {
+            return new List<string>
+            {
+                "Data 1",
+                "Data2"
+            };
+        }
+    }
+}
